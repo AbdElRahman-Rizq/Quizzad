1a1855bad0f65ee3063c11e57ed80ae1b6e63850
Rizq <a.rezk453@gmail.com>
2023-10-17 15:00:09 +0300

    delete ThemedButton line of import

--- src/components/LeaveAMessage.jsx
+++ src/components/LeaveAMessage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
-import ThemedButton from "./ThemedButton";
 
 function LeaveAMessage() {
   return (
