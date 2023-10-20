c6fdb18470ffd36fce63e75173edc0b6700b1d1c
Rizq <a.rezk453@gmail.com>
2023-10-20 21:21:57 +0300

    Update admin dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import AdminSidebar from '../components/layout/AdminSidebar'
 import { Col, Container, Row } from 'react-bootstrap'
+import Navbar from '../components/layout/Navbar'
+import NewRequest from '../components/layout/NewRequest'
 
 
 function AdminDashboard() {
   return (
     <>
-        <Row>
+        <Row className="ps-2">
             <Col lg={4}>
     <AdminSidebar/>  
+            </Col>
+            <Col lg={8} className="col-md-8 pt-4 rightSideDashboard ps-2">
+    <Navbar/>  
+    <NewRequest/>
             </Col>
         </Row>
     </>
