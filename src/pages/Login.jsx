774a768d331f19b9fb92aa1e8a668487b75e11b6
ahmedelwaraky <ahmed.elwaraky1@gmail.com>
2023-10-21 22:22:49 +0300

    Register & Login Validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,49 @@ import React from 'react'
 import '../style/Login.css'
 import Logo from '../assets/images/logo.png'
 import Exam from '../assets/images/Exams-bro.png'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
+import { FormikConsumer, useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export default function Login() {
+
+    let validationLoginSchema=Yup.object({
+        email : Yup.string()
+        .required('Email is required')
+        .email('Enter a valid email address'),
+        password :Yup.string()
+        .required('First Name is required')
+        .min(2, 'First Name must be at least 2 characters')
+        .max(10, 'First Name must be at most 10 characters'),
+    })
+
+
+    function loginSubmit(values){
+        console.log(values);
+    }
+
+    let formik = useFormik({
+        initialValues:{
+            email:"",
+            password:"",
+        },
+        onSubmit:loginSubmit ,
+        validationSchema : validationLoginSchema        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
         <section className='login sec-gap'>
@@ -12,7 +52,7 @@ export default function Login() {
                 <img src={Exam} className="w-25" alt=''/>  
             </div>
             <div className="container">
-                <form className="form  w-50 bg-light rounded-5 px-5 py-3">
+                <form onSubmit={formik.handleSubmit } className="form  w-50 bg-light rounded-5 px-5 py-3">
                     <div className='main-img'>
                         <img src={Logo} className="w-75" alt=''/>  
                     </div>
@@ -21,19 +61,21 @@ export default function Login() {
                     </div>
                     <div className='row'>                        
                         <div className='email my-2'>
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control rounded-5" id="email" placeholder=""/> 
+                            <label htmlFor="email" class="form-label">Email</label>
+                            <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} type="email" class="form-control rounded-5" id="email" placeholder=""/>
+                            {formik.errors.email && formik.touched.email && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.email}</div>)}
                         </div>
                         <div className='password my-2'>
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control rounded-5" id="password" placeholder=""/> 
+                            <label htmlFor="password" class="form-label">Password</label>
+                            <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password} type="password" class="form-control rounded-5" id="password" placeholder=""/> 
+                            {formik.errors.password && formik.touched.password && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.password}</div>)}
                         </div>
                     </div>
                     <div className='my-2'>
                         <span>You don't have an account? <Link className='text-dark' to={"/register"}>Sign up</Link></span>
                     </div>
                     <div  className='signupBtn'>
-                        <button className='btn rounded-5 col-12 text-white'>Login</button>
+                        <button  disabled={!(formik.isValid && formik.dirty)} type="submit" className='btn rounded-5 col-12 text-white'>Login</button>
                     </div>
                 </form>           
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import  '../style/Register.css'
 import Logo from '../assets/images/logo.png'
 import Exam from '../assets/images/Exams-bro.png'
-import { useFormik } from 'formik'
+import { FormikConsumer, useFormik } from 'formik'
 import * as Yup from 'yup'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
@@ -10,139 +10,145 @@ import axios from 'axios'
 
 
 export default function Register() {
-    let [errMsg, setErrMsg] = useState("")
-    let [loading, setloading] = useState(false)
-    let baseUrl = "https://route-ecommerce.onrender.com"
-    let naviagate = useNavigate()
 
-
-    
-    let validationSchema = Yup.object(
-        {
-            firstName: Yup.string().required().min(2, "Char min 2").max(10, "max Char 10"),
-            lastName : Yup.string().required().min(2, "Char min 2").max(10, "max Char 10"),
-            email:Yup.string().required().email("Enter Valid Email"),
-            phoneNumber:Yup.string().required().matches(/^[A-Z][a-zA-Z0-9!@#$%^&*()_-]{3,16}$/, "enter valid password"),
-            password:Yup.string().required().matches(/^(010|011|015|012)[0-9]{8}$/, "enter valid Phone"),
-        }
-    )
-
-let formik =useFormik({
-    initialValues: 
-    {
-        firstName:"",
-        lastName:"",
-        email:"",
-        phoneNumber:"",
-        password:"",
-    },
-    onSubmit: (data) => {
-        sendDataRegister(data)
-    },
-})
-
-async function sendDataRegister(objData) {
-
-    setloading(true)
-    let { data } = await axios.post(`${baseUrl}/api/v1/auth/signup`, objData).catch((errrr) => {
-        console.log(errrr.response.data.errors.msg);
-        setErrMsg(errrr.response.data.errors.msg)
-        setloading(false)
+    let validateSchema = Yup.object({
+        firstName : Yup.string().required('First Name is required')
+        .min(2, 'First Name must be at least 2 characters')
+        .max(10, 'First Name must be at most 10 characters'),
+        lastName : Yup.string()
+        .required('Last Name is required')
+        .min(3, 'Last Name must be at least 3 characters')
+        .max(10, 'Last Name must be at most 10 characters'),
+        gender : Yup.string().required('Gender is required'),
+        type : Yup.string().required('Gender is required'),
+        // grade : Yup.required(),
+        // birthdata : Yup.required(),
+        phoneNumber : Yup.string()
+        .required('Phone Number is required')
+        .matches(/^(010|011|015|012)[0-9]{8}$/, 'Enter a valid Egyptian phone number'),
+        email : Yup.string()
+        .required('Email is required')
+        .email('Enter a valid email address'),
+        password :Yup.string()
+        .required('First Name is required')
+        .min(2, 'First Name must be at least 2 characters')
+        .max(10, 'First Name must be at most 10 characters'),
+        // repassword : Yup.string().oneOf([Yup.ref("password")] , password and repassword don't matched).required(),
     })
-    console.log(data);
-    setloading(false)
-    if (data.message == "success") {
-      // Login
-        naviagate('/login')
+
+    function registerSubmit(values){
+        console.log(values);
     }
-}
 
+    //Formik
+    let formik = useFormik({
+        initialValues:{
+            firstName : "",
+            lastName : "",
+            gender:"",
+            type:"",
+            grade:"",
+            birthdata:"",
+            phoneNumber :"",
+            email:"",
+            password:"",
+        },
+        onSubmit:registerSubmit ,
+        validationSchema : validateSchema        
+    });
 
-
-return (
-    <div>
-        <section className='register sec-gap'>
-            <div className='exam '>
-                <img src={Exam} className="w-25" alt=''/>  
-            </div>
-            <div className="container">
-
-            <form onSubmit={formik.handleSubmit} className="form  w-50 bg-light rounded-5 px-5 py-3">
-                <div className='main-img'>
-                    <img src={Logo} className="w-75" alt=''/>  
+    return (
+        <div>
+            <section className='register sec-gap'>
+                <div className='exam '>
+                    <img src={Exam} className="w-25" alt=''/>  
                 </div>
-                <div className='main-header text-center pb-3'>
-                    <h1>Sign up Your Account</h1>
-                </div>
-                <div className='row'>
-                    <div className="col-md-6">
-                        <div className='firstName py-2'>
-                            <label for="firstName" class="form-label">First Name</label>
-                            <input onChange={formik.handleChange} type="text" class="form-control rounded-5 " id="firstName" placeholder=""/>
-                        </div>                          
-                        <div className='grade py-2'>
-                            <label for="gender" class="form-label">Gender</label>
-                            <input type="text" class="form-control rounded-5" id="gender" placeholder=""/>
-                        </div>                
-                        <div className='grade py-2'>
-                            <label for="grade" class="form-label">Grade Level | Specialization</label>
-                            <select class="form-select rounded-5" aria-label="Default select example">
-                                <option value="1">Select your grade</option>
-                                <option value="1">Grade 1</option>
-                                <option value="2">Grade 2</option>
-                                <option value="2">Grade 3</option>
-                                <option value="2">Grade 4</option>
-                                <option value="2">Grade 5</option>
-                                <option value="2">Grade 6</option>
-                                <option value="2">Grade 7</option>
-                                <option value="2">Grade 8</option>
-                                <option value="2">Grade 9</option>
-                            </select>
-                        </div>                
+                <div className="container">
+                <form onSubmit={formik.handleSubmit } className="form  w-50 bg-light rounded-5 px-5 py-3">
+                    <div className='main-img'>
+                        <img src={Logo} className="w-75" alt=''/>  
                     </div>
+                    <div className='main-header text-center pb-3'>
+                        <h1>Sign up Your Account</h1>
+                    </div>
+                    <div className='row'>
+                        <div className="col-md-6">
+                            <div className='firstName py-2'>
+                                <label htmlFor="firstName" class="form-label">First Name</label>
+                                <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.firstName} type="text" class="form-control rounded-5 " id="firstName" name="firstName"/>
+                                {formik.errors.firstName && formik.touched.firstName && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.firstName}</div>)}               
+                            </div>                          
+                            <div className='gender py-2'>
+                                <label htmlFor="gender" class="form-label">Gender</label>
+                                <select class="form-select rounded-5" name="gender" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.gender}>
+                                    <option value="0">Select your gender</option>
+                                    <option value="1">Male</option>
+                                    <option value="2">Female</option>
+                                </select>
+                            </div>                
+                            <div className='grade py-2'>
+                                <label htmlFor="grade" class="form-label">Grade Level | Specialization</label>
+                                <select class="form-select rounded-5" name="grade" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.grade}>
+                                    <option value="0">Select your grade</option>
+                                    <option value="1">Grade 1</option>
+                                    <option value="2">Grade 2</option>
+                                    <option value="2">Grade 3</option>
+                                    <option value="2">Grade 4</option>
+                                    <option value="2">Grade 5</option>
+                                    <option value="2">Grade 6</option>
+                                    <option value="2">Grade 7</option>
+                                    <option value="2">Grade 8</option>
+                                    <option value="2">Grade 9</option>
+                                </select>
+                            </div>                
+                        </div>
 
-                    <div className="col-md-6">
-                        <div className='lastName py-2'>
-                            <label for="lastName" class="form-label">Last Name</label>
-                            <input onChange={formik.handleChange} type="text" class="form-control rounded-5" id="lastName" placeholder=""/>
-                        </div>     
-                        <div className='type py-2'>
-                            <label for="type" class="form-label">Type</label>
-                            <select class="form-select rounded-5" aria-label="Default select example">
-                                <option value="1">Select you type</option>
-                                <option value="1">Teacher</option>
-                                <option value="2">Student</option>
-                            </select>
-                        </div>  
-                        <div className='birthdata py-2'>
-                            <label for="birthdata" class="form-label">Birthdate</label>
-                            <input type="date" class="form-control rounded-5" id="birthdata" placeholder=""/>
-                        </div>     
-                    </div>
+                        <div className="col-md-6">
+                            <div className='lastName py-2'>
+                                <label htmlFor="lastName" class="form-label">Last Name</label>
+                                <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.lastName} type="text" class="form-control rounded-5" id="lastName" name="lastName"/>
+                                {formik.errors.lastName && formik.touched.lastName &&(<div className='alert mt-2 p-2 alert-danger'>{formik.errors.lastName}</div>)}                        
+                            </div>     
+                            <div className='type py-2'>
+                                <label htmlFor="type" class="form-label">Type</label>
+                                <select onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.type} class="form-select rounded-5" name="type">
+                                    <option value="0">Select you type</option>
+                                    <option value="1">Teacher</option>
+                                    <option value="2">Student</option>
+                                </select>
+                            </div>  
+                            <div className='birthdata py-2'>
+                                <label htmlFor="birthdata" class="form-label">Birthdate</label>
+                                <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.birthdata} type="date" class="form-control rounded-5" id="birthdata" name="birthdata"/>
+                            </div>     
+                        </div>
 
-                    <div className='phoneNumber py-2'>
-                        <label for="phoneNumber" class="form-label">Phone Number</label>
-                        <input onChange={formik.handleChange} type="tel" class="form-control rounded-5" id="phoneNumber" placeholder=""/> 
-                    </div>
+                        <div className='phoneNumber py-2'>
+                            <label htmlFor="phoneNumber" class="form-label">Phone Number</label>
+                            <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.phoneNumber} type="tel" class="form-control rounded-5" id="phoneNumber" name="phoneNumber"/> 
+                            {formik.errors.phoneNumber && formik.touched.phoneNumber && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.phoneNumber}</div>)}
+                        </div>
 
-                    <div className='email py-2'>
-                        <label for="email" class="form-label">Email</label>
-                        <input onChange={formik.handleChange} type="email" class="form-control rounded-5" id="email" placeholder=""/> 
+                        <div className='email py-2'>
+                            <label htmlFor="email" class="form-label">Email</label>
+                            <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} type="email" class="form-control rounded-5" id="email" name="email" />
+                            {formik.errors.email && formik.touched.email && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.email}</div>)}
+                        </div>
+                        <div className='password py-2'>
+                            <label htmlFor="password" class="form-label">Password</label>
+                            <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.password} type="password" class="form-control rounded-5" id="password" name="password"/> 
+                            {formik.errors.password && formik.touched.password && (<div className='alert mt-2 p-2 alert-danger'>{formik.errors.password}</div>)}
+                        </div>
                     </div>
-                    <div className='password py-2'>
-                        <label for="password" class="form-label">Password</label>
-                        <input onChange={formik.handleChange} type="password" class="form-control rounded-5" id="password" placeholder=""/> 
+                    <div className='py-2'>
+                        <span>You have an account? <Link to={'./Login'} className='text-dark'>Sign in</Link></span>
                     </div>
+                    <div  className='signupBtn'>
+                        <button disabled={!(formik.isValid && formik.dirty)} type="submit" className='btn rounded-5 col-12 text-white'>Sign Up</button>
+                    </div>
+                    </form>
                 </div>
-                <div className='py-2'>
-                    <span>You have an account? <Link to={"/login"} className='text-dark'>Sign in</Link ></span>
-                </div>
-                <div  className='signupBtn'>
-                    <button disabled={!formik.isValid} className='btn rounded-5 col-12 text-white'>Sign Up</button>
-                </div>
-                </form>
-            </div>
-        </section>
-    </div>
-)
+            </section>
+        </div>
+    )
 }
