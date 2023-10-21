36d24611ba191a96e6ea617af83dd88841ef707c
Rizq <a.rezk453@gmail.com>
2023-10-21 23:06:00 +0300

    Add century font

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.json'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
