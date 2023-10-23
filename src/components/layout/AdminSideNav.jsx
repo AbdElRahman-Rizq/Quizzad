537000bfec7be5d18bd6ac6b9483a840f294c72a
MOhamed Al-Shafiy <mohamed.basyoni02@gmail.com>
2023-10-23 14:23:05 +0300

    sideBar

--- src/components/layout/AdminSideNav.jsx
+++ src/components/layout/AdminSideNav.jsx
@@ -1,39 +1,93 @@
-import React from 'react'
-import '../../assets/bootstrap/css/bootstrap.min.css'
-import logo from '../../assets/images/logo.svg'
-import '../../assets/css/dashboard.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/images/logo.svg';
+import '../../assets/css/dashboard.css'; // Import your custom CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 export function AdminSideNav() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div>
-        <nav className="navbar align-items-start sidebar sidebar-dark accordion sideBar navbar-dark">
-            <div className="container-fluid d-flex flex-column p-0 ">
-                <a className="navbar-brand d-flex justify-content-center py-5 align-items-center sidebar-brand m-0" href="#">
-                    <div className="sidebar-brand-icon rotate-n-15"><img className='ZADlogo' src={logo} alt="quizZAD" /></div>
-                    <div className="sidebar-brand-text mx-3"><span >QuizZAD</span></div>
-                </a>
-            <hr className="sidebar-divider my-2" />
-            <ul className="navbar-nav text-light w-" id="accordionSidebar">
-                <li className="nav-item">
-                    <div className='bg-light p-3 rounded-5 m-2 d-flex align-iteams-center justify-content-evenly'>
-                        <div>
-                            <i className="sideBarIcon fa fa-light fa-house-user fs-3" ></i>
-                        </div>
-                            <span className='text-dark fs-5'>Dashboard</span>
-                    </div>
-                    </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="table.html"><i className="fas fa-table" /><span>Class</span></a></li>
-                <li className="nav-item">
-                    <a className="nav-link" href="login.html"><i className="far fa-user-circle" /><span>Quiz</span></a></li>
-                <li className="nav-item">
-                    <a className="nav-link" href="register.html"><i className="fas fa-user-circle" /><span>User</span></a></li>
-            </ul>
-            <div className="text-center d-none d-md-inline"><button className="btn rounded-circle border-0" id="sidebarToggle" type="button" /></div>
-            </div>
+      {isMobile ? (
+        // Render a mobile navbar here
+        <nav className="navbar navbar-expand-lg">
+          <a className="navbar-brand mx-3" href="#">
+            <img className="ZADlogo" src={logo} alt="quizZAD" />
+            <span className=' mx-2 align-iteams-center'>QuizZAD</span> 
+          </a>
+          <div className='dropdown'>
+          <button className="navbar-toggler dropbtn mx-3  bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon " />
+
+          </button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          </div>
+
         </nav>
+      ) : (
+        // Render the sidebar for larger screens here
+        <nav className="navbar align-items-start sidebar accordion sideBar p-0 border rounded-5">
+          <div className="container-fluid d-flex flex-column p-0 py-5">
+          <a className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+            <div className="sidebar-brand-icon">
+              <img className='ZADlogo' src={logo} alt="quizZAD" />
+            </div>
+            <div className="sidebar-brand-text mx-4">
+              <span className='text-light fw-bold fs-4'>QuizZAD</span>
+            </div>
+          </a>
+          <hr className="sidebar-divider" />
+          <ul className="navbar-nav text-light py-1" id="accordionSidebar">
+            <li className="nav-item my-3 SBphase">
+              <a className="nav-link p-3" href="profile.html">
+                <i className="fa fa-light fa-house-user fs-4" style={{color: "#89288F",}}></i>
+                <span className='fs-5 px-3 text-dark'>Dashboard</span>
+              </a>
+            </li>
+            <li className="nav-item my-3 SBphase">
+              <a className="nav-link p-3" href="table.html">
+                <i className="fa fa-solid fa-landmark fs-4" style={{color: "#89288F",}} />
+                <span className='fs-5 px-3 text-dark'>Class</span>
+              </a>
+            </li>
+            <li className="nav-item my-3 SBphase">
+              <a className="nav-link p-3" href="login.html">
+                <i className="fa-solid fa-book fs-4 " style={{color: "#89288F",}} />
+                <span className='fs-5 px-3 text-dark'>Quiz</span>
+              </a>
+            </li>
+            <li className="nav-item my-3 SBphase">
+              <a className="nav-link p-3" href="register.html">
+                <i className="fa fa-solid fa-user fs-4 " style={{color: "#89288F",}} />
+                <span className='fs-5 px-3 text-dark'>User</span>
+              </a>
+            </li>
+          </ul>
+          <div className="text-center d-none d-md-inline">
+            <button className="btn rounded-circle border-0" id="sidebarToggle" type="button" />
+          </div>
+        </div>
+          
+        </nav>
+      )}
     </div>
-  )
+  );
 }
+
+
+
