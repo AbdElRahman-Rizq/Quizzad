f34f1c2fc75f6d403b878fe6e47f0c41836b385a
ahmedelwaraky <ahmed.elwaraky1@gmail.com>
2023-10-26 21:10:59 +0300

    f

--- src/components/layout/Topnav.jsx
+++ src/components/layout/Topnav.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import vector from '../../assets/images/teacher.png'
+import vector from '../../assets/images/profile.png'
 import "../../assets/css/Topnav.css"
 
 
 export default function Topnav() {
     return (
         <div>
-            <div className='container '>
-                <div class="hstack gap-3 bg-lihgt">
+            <div className='containe '>
+                <div class="hstack gap-3 bg-lihgt mb-2">
                     <div class=" title">Administrator</div>
-                    <div class="pe-2 ms-auto"><img className="adminVector" src={vector}></img></div>
+                    <div class="pe-4 ms-auto"><img className="adminVector" src={vector}></img></div>
                 </div>
             </div>
         </div>
