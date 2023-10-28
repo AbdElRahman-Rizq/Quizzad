b7e5837712611a76c3602bed2e8d29527a2bbc5e
Rizq <a.rezk453@gmail.com>
2023-10-28 02:35:50 +0200

    Creating Student view & quizes for student

---
+++ src/components/studentComponents/quiz/QuizPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { QuizNav } from '../../adminComponents/Quiz/QuizNav'
+
+function QuizPage() {
+  return (
+    <div>
+        <QuizNav/>
+    </div>
+  )
+}
+
+export default QuizPage
