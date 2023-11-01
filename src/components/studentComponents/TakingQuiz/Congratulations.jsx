09ca547ff4855ef79fd9698a3c9c0cfadb3c901e
Rizq <a.rezk453@gmail.com>
2023-11-01 02:22:40 +0200

    Change Taking quiz pages

--- src/components/studentComponents/TakingQuiz/Congratulations.jsx
+++ src/components/studentComponents/TakingQuiz/Congratulations.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import NavForResult from './NavForResult'
-import QuestionForm from '../../teacherComponents/Quiz/QuestionForm'
 
 function Congratulations() {
   return (
     <div>
         <NavForResult/>
-        <QuestionForm/>
     </div>
   )
 }
--- src/components/studentComponents/TakingQuiz/TakeQuiz.jsx
+++ src/components/studentComponents/TakingQuiz/TakeQuiz.jsx
@@ -1,27 +1,94 @@
-import React, { useState } from 'react'
-import NavForQuiz from './NavForQuiz'
-import QuestionForm from '../../teacherComponents/Quiz/QuestionForm'
-import ThemedButton from '../../layout/ThemedButton'
+import React, { useState, useRef, useEffect } from 'react';
+import NavForQuiz from './NavForQuiz';
+import QuestionForm from '../../teacherComponents/Quiz/QuestionForm';
+import Slider from 'react-slick';
 
 function TakeQuiz() {
-  const [questionNubmer , setQuestionNubmer]=useState(0)  
-  const [questionPage , setQuestionPage]=useState(0)  
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [questionPage, setQuestionPage] = useState(0);
+  const sliderRef = useRef(null);
+  const [timer, setTimer] = useState(30); 
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+  };
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 1000);
+
+    if (timer === 0) {
+      clearInterval(timerInterval);
+      // Navigate to the desired location when the timer runs out
+      window.location.href = '/';
+    }
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [timer]);
+   // Format the remaining time as minutes and seconds (MM:SS)
+  const formattedTime = `${Math.floor(timer / 60)
+  .toString()
+  .padStart(2, '0')}:${(timer % 60).toString().padStart(2, '0')}`;
+  const handleNext = () => {
+    setQuestionNumber((prevNumber) => prevNumber + 1);
+    sliderRef.current.slickNext();
+  };
+
+  const handlePrevious = () => {
+    sliderRef.current.slickPrev();
+  };
 
   return (
     <div>
-        <NavForQuiz questionNubmer={questionNubmer}/>
-{/* Questions */}
-{questionPage==0?
-  <>
-  <QuestionForm/>
-  </>:null
-}
-<div className='mx-4 mb-4'>
-  
-<ThemedButton content={"Next"} propWidth={"15rem"} propPadding={".4rem 0rem"}/>
-</div>
+      <NavForQuiz questionNumber={questionNumber} timer={formattedTime} />
+      {/* Questions */}
+      <Slider ref={sliderRef} {...settings}>
+        <div key={1}>
+          <QuestionForm />
+
+          <div className='my-3 m-auto col-md-6'>
+            <button
+              type='submit'
+              className='quizButton rounded-4 p-3 w-100 fs-5'
+              onClick={handleNext}
+            >
+              Next
+              <i className='fa-solid fa-arrow-right ms-3' />
+            </button>
+          </div>
+        </div>
+        <div key={2}>
+          <div className='col-md-8 m-auto'>
+            <hr />
+          </div>
+
+          {/*---------------------- Second question --------------------------*/}
+
+          <QuestionForm />
+
+          <div className='my-3 m-auto col-md-6'>
+            <button
+              type='submit'
+              className='quizButton rounded-4 p-3 w-100 fs-5'
+              onClick={handlePrevious}
+            >
+              <i className='fa-solid fa-arrow-left me-3' />
+              Back
+            </button>
+          </div>
+        </div>
+      </Slider>
     </div>
-  )
+  );
 }
 
-export default TakeQuiz
+export default TakeQuiz;
