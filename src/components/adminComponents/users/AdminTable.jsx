71cd6c85378c8e5fc9ac390b43e96cd6bd9d2046
Rizq <a.rezk453@gmail.com>
2023-11-02 05:37:41 +0200

    Edit vite.config

--- src/components/adminComponents/users/AdminTable.jsx
+++ src/components/adminComponents/users/AdminTable.jsx
@@ -1,70 +1,72 @@
 import React, { useEffect, useState } from "react";
 import { Circles } from "react-loader-spinner";
-import "../../../assets/css/Users.css"
+import "../../../assets/css/Users.css";
 import { Container, NavLink, Table } from "react-bootstrap";
 
 export default function AdminTable() {
-    const [requests, setRequests] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((data) => {
-                setRequests(data);
-            })
-            .catch((err) => {
-            console.log("err:", err);
-            });
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/users/")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+        setLoading(false);
+      });
+  }, []);
 
-    if (requests.length === 0) {
+  if (loading) {
     return (
-    <div id="loading">
+      <div id="loading">
         <Circles
-            height={500}
-            width={60}
-            color="#89288F"
-            ariaLabel="circles-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
+          height={500}
+          width={60}
+          color="#89288F"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
         />
-    </div>
+      </div>
     );
-}
+  }
 
-return (
-    <Container >
-        <Table hover responsive className=" userTable">
+  return (
+    <Container>
+      <Table hover responsive className=" userTable">
         <thead className="custom-thead">
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Role</th>
-                <th>Specific</th>
-                <th>Email</th>
-                <th>Actions</th>
-            </tr>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody className="userBodyTable">
-            {requests.map((request) => ( request.id<8 ?<tr key={request.id}>
-                <td>{request.id}</td>
-                <td>{request.name}</td>
-                <td>{request.username}</td>
-                <td>{request.phone}</td>
-                <td>{request.email}</td>
-                <td>
-                    <div className="p-0">
-                    <NavLink className="fa-solid fa-check mx-3 fs-3 text-success "/>
-                    <NavLink className="fa-solid fa-xmark mx-3 fs-3 text-danger " />
-                    </div>
-                </td>
-                </tr>
-                : null
-
-            ))}
-            </tbody>
-        </Table>
+          {users.slice(0, 8).map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
+                <div className="p-0">
+                  <NavLink className="fa-solid fa-check mx-3 fs-3 text-success" />
+                  <NavLink className="fa-solid fa-xmark mx-3 fs-3 text-danger" />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
-);
+  );
 }
