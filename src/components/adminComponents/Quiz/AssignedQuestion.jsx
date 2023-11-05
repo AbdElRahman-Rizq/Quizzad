4356a1d2591657931098ae88d60c141f2f89eeaf
MOhamed Al-Shafiy <mohamed.basyoni02@gmail.com>
2023-11-05 06:49:08 +0200

    props id

--- src/components/adminComponents/Quiz/AssignedQuestion.jsx
+++ src/components/adminComponents/Quiz/AssignedQuestion.jsx
@@ -1,19 +1,14 @@
 import { Accordion } from 'react-bootstrap';
 import '../../../assets/css/quiz.css'
 import { useQuizLogic } from '../../../controls/QuizLogic';
-import { useParams } from "react-router-dom";
 
 export function AssignedQuestion() {
-  const {  handleAddQuestionClick, questions } = useQuizLogic();
-  let { id } = useParams();
-  id = parseInt(id) || 0;
+  const {  handleAddQuestionClick, questions,id } = useQuizLogic();
 
-  console.log(id);
     return (
           <div className='m-2 mt-3'>
           <section className="rounded-4 py-4 bg-light Scroller" >
 
-          <form className='mx-2 m-auto rounded-4' encType="multipart/form-data">
             <div className="container p-0">
               <div className="row mb-4">
                 <div className="col-md-4 col-xl-10 text-center m-auto p-2 mt-1 rounded-4">
@@ -45,7 +40,6 @@ export function AssignedQuestion() {
                     <button type="submit" className="quizButton rounded-4 fs-4 p-3 w-100">
                     {id == 0 ? 'Create New Quiz' : 'Save Changes'}</button>
                   </div>
-                </form>
               </section>
             </div>
           );
